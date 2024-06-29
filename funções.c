3e86568fec2064ab8a7106b5f335dde2ee3c7709
Gabriel Detofol <detofol29@gmail.com>
2024-06-29 18:19:57 -0300

    Adicionando a função cabeçalho comum; Renomeando as funções no padrao CamelCase; Indentando o código;

--- funções.c
+++ funções.c
@@ -1,46 +1,66 @@
 #include <stdio.h>
 #include <math.h>
 
-double calcular_pic(int n, double p[], double q[]) {
-    double soma_q = 0, soma_p = 0;
-    for (int i = 0; i < n; i++) {
-        soma_q += q[i];
-        soma_p += p[i];
-    }
-    if (soma_p > 0) {
-        return (soma_q / soma_p) * 100.0;
-    } else {
-        return 0.0;
-    }
+//----------FUNCOES DE CALCULO----------
+
+double calcularPic(int n, double p[], double q[])
+{
+  double soma_q = 0, soma_p = 0;
+
+  for (int i = 0; i < n; i++)
+  {
+    soma_q += q[i];
+    soma_p += p[i];
+  }
+
+  if (soma_p > 0)
+  {
+    return (soma_q / soma_p) * 100.0;
+  }
+  else
+  {
+    return 0.0;
+  }
 }
 
-double calcularGuc(double *p, double *q, double *u, int n){
-    double numerador = 0, denominador = 0, guc = 0;
+double calcularGuc(double *p, double *q, double *u, int n)
+{
+  double numerador = 0, denominador = 0, guc = 0;
  
-    for(int i = 1; i <= n; i++)
-    {
-        numerador += u[i] * (p[i] - q[i]);
-    }
-    for(int i = 1; i <= n; i++)
-    {
-        denominador += p[i] - q[i];
-    }
-    
-    guc = numerador / denominador;
+  for(int i = 1; i <= n; i++)
+  {
+    numerador += u[i] * (p[i] - q[i]);
+  }
 
-    return guc;
+  for(int i = 1; i <= n; i++)
+  {
+    denominador += p[i] - q[i];
+  }
+    
+  guc = numerador / denominador;
+
+  return guc;
 }
 
-void rodape_comum() {
+//----------FUNCOES DE EXIBICAO----------
 
-    printf("---------------------------------------------------------------------------------------------------------------------------------- \n");
-    printf("Programa desenvolvido pelos alunos. \n");
-    printf("-Vitor Vitorette Serafim de Pina; \n");
-    printf("-Humberto Henry Gontijo Braga; \n");
-    printf("-Yuri Salatiel de Lima; \n");
-    printf("-Gabriel Augusto Ramos Detofol; \n");
-    printf("-Leo Faris Monteiro de Paula. \n");
-    }
+void cabecalhoComum()
+{
+  printf("\nUFG-BSI-IP (COOPERATIVA AGRÍCOLA GRÃO_DO_VALE V1.0)");
+  printf("\nANO: 2024 <nome da funcao>"); //nome da funcao a ser definida
+  printf("\n---------------------------------------------...------\n");
+}
+
+void rodapeComum() 
+{
+  printf("---------------------------------------------------------------------------------------------------------------------------------- \n");
+  printf("Programa desenvolvido pelos alunos\n");
+  printf("-Vitor Vitorette Serafim de Pina;\n");
+  printf("-Humberto Henry Gontijo Braga;\n");
+  printf("-Yuri Salatiel de Lima;\n");
+  printf("-Gabriel Augusto Ramos Detofol;\n");
+  printf("-Leo Faris Monteiro de Paula.\n");
+}
 
 void interface()
 {
@@ -59,20 +79,20 @@ void menu()
   do
   {
     scanf("%d", &codigo);
-  switch (codigo)
-  {
-  case 1:  
-    interface();
-    break;
-  case 2:  
-    interface();
-    break;
-  case 3:
-    printf("\nfinalizando o programa...\n\n");
-    break;
-  default:
-    printf("\nCódigo não encontrado...\n");
-    break;
-  }
+    switch (codigo)
+    {
+      case 1:  
+        interface();
+        break;
+      case 2:  
+        interface();
+        break;
+      case 3:
+        printf("\nfinalizando o programa...\n\n");
+        break;
+      default:
+        printf("\nCódigo não encontrado...\n");
+        break;
+    }
   } while ( codigo != 3);
 }
